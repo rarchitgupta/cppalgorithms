3f55a75214276cb97294a976124da235c8ae47b4
Archit <rarchitgupta@gmail.com>
2020-01-08 16:28:47 +0530

    String permutations program

--- Week 1/test.cpp
+++ Week 1/test.cpp
@@ -1,10 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int stringpermute(string a,int l,int r)
+{
+	if (l == r)  
+        cout<<a<<endl;  
+    else
+    {  
+        int i;  
+        for (i = l; i <= r; i++)  
+        {  
+  
+            // Swapping done  
+            swap(a[l], a[i]);  
+  
+            // Recursion called  
+            stringpermute(a, l+1, r);  
+  
+            //backtrack  
+            swap(a[l], a[i]);  
+        }  
+    }  
+}
+
 int main()
 {
-	cout<<"Hello World!"<<endl;
-	
-	return 0; 
+	string x;
+	cout<<"Enter string:"<<endl;
+	cin>>x;
+
+	int n=x.size();
+	cout<<"Permutations: "<<endl;
+	stringpermute(x,0,n-1);
 }
